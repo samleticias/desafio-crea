e30856953b8f5d6e346835c12f559fec5d80e6d8
samleticias <sammyaleticia09@gmail.com>
2024-05-25 02:28:51 -0300

    feat: implementing methods to verify the persistence of the professional in the database

--- src/main/java/samleticias/desafiocreaapi/services/ProfessionalService.java
+++ src/main/java/samleticias/desafiocreaapi/services/ProfessionalService.java
@@ -41,7 +41,7 @@ public class ProfessionalService {
             throw new AlreadyExistException("Já existe um profissional com o e-mail informado.");
         }
         Professional professional = new Professional(professionalDTO);
-        verifyProfessionalType(professional);
+        checkProfessionalType(professional);
 
         this.saveProfessional(professional);
         return professional;
@@ -76,21 +76,22 @@ public class ProfessionalService {
         professionalRepository.delete(professional.get());
     }
 
-    private void verifyProfessionalType(Professional professional) throws InvalidActionException {
+    // verifica tipo do profissional, se é visado ou registrado
+    private void checkProfessionalType(Professional professional) throws InvalidActionException {
         if(professional.getProfessionalType() == ProfessionalType.REGISTERED && professional.getVisaDate() != null){
-            throw new InvalidActionException("Não é permitido adicionar data de visto para um profissional do tipo registrado");
+            throw new InvalidActionException("Não é possível atribuir uma data de visto a um profissional com tipo registrado.");
         }
         if(professional.getProfessionalType() == ProfessionalType.ENDORSED && professional.getRegistrationDate() != null){
-            throw new InvalidActionException("Não é permitido adicionar data de registro para um profissional do tipo visado");
+            throw new InvalidActionException("Não é atribuir data de registro para um profissional do tipo visado");
         }
     }
 
     public Professional update(Professional professionalToEdit) throws ProfessionalNotFoundException, InvalidActionException {
         Professional professionalBefore = findById(professionalToEdit.getId());
 
-        verifyProfessionalType(professionalToEdit);
+        checkProfessionalType(professionalToEdit);
 
-        // verifica e-mail
+        // verifica email
         if(!professionalToEdit.getEmail().equals(professionalBefore.getEmail())){
             if(professionalRepository.findProfessionalByEmail(professionalToEdit.getEmail()).isPresent()){
                 throw new InvalidActionException("O email fornecido já está sendo utilizado");
@@ -124,18 +125,21 @@ public class ProfessionalService {
         Professional professional = professionalAlternative.get();
         Title title = titleAlternative.get();
 
-
+        // verifica se profissional possui registro cancelado
         if(isProfessionalCancelled(professional))
         {
             throw new InvalidActionException("Não é possível atribuir um título a um profissional com registro cancelado.");
         } else {
             if(professional.getRegistrationStatus() == null)
             {
+                // realiza ativação do registro
+
                 activateProfessionalStatus(professional);
                 professional.setUniqueCode();
             }
             if(checkProfessionalTitle(professional, title))
             {
+                // verifica se o profissional já possui o título a ser verificado
                 throw new DuplicateResourceException("O título informado já está associado ao profissional.");
             }
             professional.assignProfessionalTitle(title);
@@ -156,22 +160,54 @@ public class ProfessionalService {
         return professional;
     }
 
+    public Professional disableProfessional(Integer id) throws ProfessionalNotFoundException, ExistingItemException {
+        Professional professional = findById(id);
+
+        // verifica se o profissional já possui registro inativo
+        if(checkProfessionalInactivity(professional)) throw new ExistingItemException("O profissional já é inativo.");
+
+        professional.setRegistrationStatus(RegistrationStatus.INACTIVE);
+        saveProfessional(professional);
+        return professional;
+    }
+
+    public Professional cancelProfessional(Integer id) throws ProfessionalNotFoundException, ExistingItemException {
+        Professional professional = findById(id);
+
+        // verifica se profissional já está cancelado
+        if(isProfessionalCancelled(professional)) throw new ExistingItemException("O profissional já está cancelado.");
+
+        professional.setRegistrationStatus(RegistrationStatus.CANCELED);
+        saveProfessional(professional);
+        return professional;
+    }
+
+    // verifica se o profissional está presente na base de dados
     private boolean checkProfessionalExistence(Professional professional){
         return professionalRepository.findById(professional.getId()).isPresent();
     }
 
+    // verifica se o profissional já possui registro ativo
     private boolean checkProfessionalActivity(Professional professional){
         return professional.getRegistrationStatus() == RegistrationStatus.ACTIVE;
     }
 
+    // verifica se profissional possui registro inativo
+    private boolean checkProfessionalInactivity(Professional professional){
+        return professional.getRegistrationStatus() == RegistrationStatus.INACTIVE;
+    }
+
+    // verifica se o profissional possui registro cancelado
     private boolean isProfessionalCancelled(Professional professional){
         return professional.getRegistrationStatus() == RegistrationStatus.CANCELED;
     }
 
+    // ativação do registro do profissional
     private void activateProfessionalStatus(Professional professional) {
         professional.setRegistrationStatus(RegistrationStatus.ACTIVE);
     }
 
+    // faz comparação para verificar se o título desejado já está na lista de tíyulos do profissional
     private boolean checkProfessionalTitle(Professional professional, Title titleForValidation){
         for(Title title : professional.getTitles()){
             if(title.equals(titleForValidation)) return true;
